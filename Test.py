51b7454b0f658f1dce432cb1bc089428a4b12ed4
Idk1017 <dikang1017@gmail.com>
2025-03-24 12:37:16 +0800

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.title("Minecraft Map Guide")
+
+# Function to draw the map
+def draw_map():
+    plt.figure()
+    plt.plot([0, 10], [0, 10], label="Path 1", color='blue')
+    plt.plot([0, 10], [10, 0], label="Path 2", color='red')
+    plt.title("Minecraft Map Guide")
+    plt.xlabel("X Coordinate")
+    plt.ylabel("Y Coordinate")
+    plt.legend()
+    plt.grid(True)
+    plt.axis("equal")
+    st.pyplot(plt.gcf())
+    plt.close()
+
+# Render the map
+draw_map()
